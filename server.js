1ff2b095486e8916f0fa8bfc4cee0222dfb45df7
Jack Yeh <jack.yeh@gmail.com>
2016-01-14 21:32:55 -0700

    add functionality: user can now broadcast to all other browsers listening on port 3000

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-var PORT = process.env.PORT || 3000;
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var PORT      = process.env.PORT || 3000;
+var express   = require('express');
+var app       = express();
+var http      = require('http').Server(app);
+var io        = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function() {
-  console.log('User connected via socket.io!')
+io.on('connection', function(socket) {
+  console.log('User connected via socket.io!');
+
+  socket.on('message', function(message) {
+    console.log('Message received: ' + message.text);
+
+    // send to everyone except for the person that sent it; io.emit sends to everyone
+    socket.broadcast.emit('message', message);
+  });
+
+    socket.emit('message', {
+      text: 'Welcome to the chat application!'
+    });
 });
 
 http.listen(PORT, function() {
