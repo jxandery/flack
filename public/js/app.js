308787121f70f400a86ede0bff283411e85bc035
Jack Yeh <jack.yeh@gmail.com>
2016-01-14 21:45:14 -0700

    add functionality: add front end form to message and reset input field to blank after submitting message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,23 @@ socket.on('message', function(message) {
   console.log('new message:');
   console.log(message.text);
 });
+
+// handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+
+  // assigns the input field to the variable message
+  // the $ makes it available throughout the app as a jquery variable
+  var $message = $form.find('input[name=message]');
+
+  // broadcasts whatever has been inputted in the message input field
+  socket.emit('message', {
+    text: $message.val()
+  });
+
+  // resets the input field to blank
+  $message.val('');
+
+});
