86c3b5fd53991124238731bd68a80c732cd3b9bc
Jack Yeh <jack.yeh@gmail.com>
2016-01-18 19:12:29 -0700

    create/pass: server returns a 200 response for valid get request

--- test/server_test.js
+++ test/server_test.js
@@ -4,3 +4,10 @@ var expect  = chai.expect;
 var request = require('supertest');
 var server  = require('../server.js');
 
+describe('server.js', function () {
+  it('GET "/" should return a 200 response', function (done) {
+    request(server)
+      .get('/')
+      .expect(200, done);
+  });
+});
